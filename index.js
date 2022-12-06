16a202f00c8d96bf9fb54519d341792097553496
11jolek11 <dabrowskiandrzej094@gmail.com>
2022-12-06 01:55:37 +0100

    sync

--- index.js
+++ index.js
@@ -44,24 +44,81 @@ function register(){
     // global_register.push(obj);
     // global_register.slice(-1)[0]  obj
     if (JSON.stringify(global_register.slice(-1)[0]) !== JSON.stringify(obj)){
-        // console.log('not doubled');
-        global_register.push(obj)
         const dev = document.getElementById('registerlist');
+        // console.log('not doubled');
+
+        // for (i = 0; i < global_register.length; i++){
+        //     if (global_register[i].uuid == obj.uuid && global_register[i].uuid == obj.uuid){
+        //         delete global_register[i]
+        //     }
+        // }
+        // FIXME: possible problem
+        // const element_to_del = document.getElementById(obj.name);
+        // element_to_del.remove();
+        global_register.push(obj)
         const payload = 
-        `<div style="border:thin" class="Item", id=${obj.name}>\
+        `<div class="item", id=${obj.name}>\
+            <input type="button" value=${obj.name} onclick="exit_generator(this)">Delete</input>\
             <p>Name: ${obj.name}</p><p>Interface IP: ${obj.ip}</p>\
             <p>Port: ${obj.port}</p>\
             <p>Interface id: ${obj.uuid}</p>\
+            <p>Config vector: ${obj.config}</p>\
             <input type="button" value=${obj.name} onclick="status_generator(this)">Status</input>\
                 <p id="status-${obj.name}"></p>\
             <input type="button" value=${obj.name} onclick="stop_generator(this)">Stop</input>\
+            <input type="button" value=${obj.name} onclick="hide(this)">Edit</input>\
+            <div id="edit-${obj.name}" class="common-edit">
+                <label for="channel">Channel: </label>
+                <input type="text" id="channel" class="_input"><br><br>
+                <label for="frequency">Frequency: </label>
+                <input type="number" id="frequency" class="_input"><br><br>
+                <p>MQTT</p>
+                <label for="mqttbroker">Broker adress: </label>
+                <input type="text" id="mqttbroker" class="_input"><br><br>
+                <label for="mqttport">Broker port</label>
+                <input type="number" id="mqttport" class="_input"><br><br>
+                <label for="mqtttopic">Topic to publish on: </label>
+                <input type="text" id="mqtttopic" class="_input"><br><br>
+                <p>HTTP</p>
+                <label for="httphost">Recipient IP adress: </label>
+                <input type="text" id="httphost" class="_input"><br><br>
+                <label for="httpport">Port: </label>
+                <input type="number" id="httpport" class="_input"><br><br>
+                <button type="submit" id="start-${obj.name}" value=${obj.name} onclick="start_generator(this)">Send</button>
+            </div>
         </div>`;
         dev.innerHTML += payload;
+    } else {
+        window.alert("Doubled config");
     };
     }  
 }
 
+// <input type="button" id="start-${obj.name}" value=${obj.name} onclick="start_generator(this)">Send</input>
+
 function stop_generator(button) {
+    // global_register.forEach(element => {
+    for (let i = 0; i < global_register.length; i++){
+        const element = global_register[i];
+        var requestOptions = {
+            method: 'POST',
+            redirect: 'follow',
+            // mode: 'no-cors',
+          };
+        if (element.name == button.value){       
+            // const element_to_del = document.getElementById(button.value);
+            // element_to_del.remove();
+            // global_register.splice(i, 1);
+            fetch(`http://${element.ip}:${element.port}/${button.value}/stop`, requestOptions)
+                .then(response => response.text())
+                .then(result => console.log(result))
+                .catch(error => console.log('error', error));
+        }
+    // });
+ };
+}
+
+function exit_generator(button) {
     // global_register.forEach(element => {
     for (let i = 0; i < global_register.length; i++){
         const element = global_register[i];
@@ -83,12 +140,7 @@ function stop_generator(button) {
  };
 }
 
-function start_generator(){
-    
-}
-
 function status_generator(button) {
-
     var requestOptions = {
         method: 'POST',
         redirect: 'follow',
@@ -109,16 +161,238 @@ function status_generator(button) {
     }
 }
 
+function hide(button) {
+    for (let i = 0; i < global_register.length; i++){
+        const element = global_register[i];
+        if (element.name == button.value){
+            var x = document.getElementById(`edit-${element.name}`);
+            if (x.style.display === "none") {
+              x.style.display = "block";
+            } else {
+              x.style.display = "none";
+            }
+        }
+    }    
+}
+
+
+
+function start_generator(button){
+    let json_parse = {}
+    // console.log('1');
+    for (let i = 0; i < global_register.length; i++){
+        var data_source = '';
+        var url = '';
+        // console.log('2');
+        let temp = [];
+        const element = global_register[i];
+        // console.log(element.name + ' - ' + button.value);
+        if (element.name == button.value){
+            console.log('#####');
+            console.log(JSON.parse(element.config).data.source);
+            console.log('#####');
+            data_source = JSON.parse(element.config).data.source;
+            // console.log('3');
+            url = `http://${element.ip}:${element.port}/${button.value}/start`;
+            var targetInput = document.getElementById(`edit-${element.name}`).getElementsByClassName("_input");
+            for (i = 0; i < targetInput.length; i++){
+                temp.push(targetInput[i].value)
+                // console.log(targetInput[i].value);
+            }
+        }
+        console.log(data_source);
+
+        json_parse = {
+            data: {
+                source: data_source,
+                channel: temp[0],
+                frequency: temp[1]
+            },
+            MQTT: {
+                broker: temp[2],
+                port: temp[3],
+                topic: temp[4]
+            },
+            HTTP: {
+                host: temp[5], 
+                port: temp[6]
+            }
+        };
+    };
+        // json_parse = JSON.parse(str_parse);
+        console.log(JSON.stringify(json_parse));
+        x = fetch(
+            url,
+            {
+                method: 'POST',
+                headers: {
+                  'Accept': 'application/json',
+                  'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(json_parse)
+                // body: str_parse,
+              });
+    }; 
+
+
+function start_generator(button){
+    let json_parse = {}
+    // console.log('1');
+    for (let i = 0; i < global_register.length; i++){
+        var data_source = '';
+        var url = '';
+        // console.log('2');
+        let temp = [];
+        const element = global_register[i];
+        // console.log(element.name + ' - ' + button.value);
+        if (element.name == button.value){
+            console.log('#####');
+            console.log(JSON.parse(element.config).data.source);
+            console.log('#####');
+            data_source = JSON.parse(element.config).data.source;
+            // console.log('3');
+            url = `http://${element.ip}:${element.port}/${button.value}/start`;
+            var targetInput = document.getElementById(`edit-${element.name}`).getElementsByClassName("_input");
+            for (i = 0; i < targetInput.length; i++){
+                temp.push(targetInput[i].value)
+                // console.log(targetInput[i].value);
+            }
+        }
+        console.log(data_source);
+
+        json_parse = {
+            data: {
+                source: data_source,
+                channel: temp[0],
+                frequency: temp[1]
+            },
+            MQTT: {
+                broker: temp[2],
+                port: temp[3],
+                topic: temp[4]
+            },
+            HTTP: {
+                host: temp[5], 
+                port: temp[6]
+            }
+        };
+    };
+        // json_parse = JSON.parse(str_parse);
+        console.log(JSON.stringify(json_parse));
+        x = fetch(
+            url,
+            {
+                method: 'POST',
+                headers: {
+                  'Accept': 'application/json',
+                  'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(json_parse)
+                // body: str_parse,
+              });
+    }; 
+
+
+function first_generator(){
+    let ip = document.getElementById('ip').value;
+    let port = document.getElementById('port').value;
+    let name = document.getElementById('name').value;
+    let json_parse = {}
+    temp = []
+    var targetInput = document.getElementById("command").getElementsByClassName("subfield");
+
+    for (i = 0; i < targetInput.length; i++){
+        temp.push(targetInput[i].value);
+    }
+    // console.log('1');
+    // for (let i = 0; i < global_register.length; i++){
+        // var data_source = '';
+        // var url = '';
+        // console.log('2');
+        // let temp = [];
+        // const element = global_register[i];
+        // console.log(element.name + ' - ' + button.value);
+        // if (element.name == button.value){
+            // console.log('#####');
+            // console.log(JSON.parse(element.config).data.source);
+            // console.log('#####');
+            // data_source = JSON.parse(element.config).data.source;
+            // // console.log('3');
+            // url = `http://${element.ip}:${element.port}/${button.value}/start`;
+            // var targetInput = document.getElementById(`man-edit-${element.name}`).getElementsByClassName("man_input");
+            // for (i = 0; i < targetInput.length; i++){
+            //     temp.push(targetInput[i].value)
+                // console.log(targetInput[i].value);
+        //     }
+        // }
+        json_parse = {
+            data: {
+                source: temp[0],
+                channel: temp[1],
+                frequency: temp[2]
+            },
+            MQTT: {
+                broker: temp[2+1],
+                port: temp[3+1],
+                topic: temp[4+1]
+            },
+            HTTP: {
+                host: temp[5+1], 
+                port: temp[6+1]
+            }
+        };
+
+        var url = `http://${ip}:${port}/${name}/start`;
+        // json_parse = JSON.parse(str_parse);
+        console.log(JSON.stringify(json_parse));
+        x = fetch(
+            url,
+            {
+                method: 'POST',
+                headers: {
+                  'Accept': 'application/json',
+                  'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(json_parse)
+                // body: str_parse,
+              });
+    }; 
 
 
 
 
 
-    // global_register.forEach(element => {
-//     for (let i = 0; i < global_register.length; i++){
-//         const element = global_register[i];
-//         var requestOptions = {
-//             method: 'POST',
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// global_register.forEach(element => {
+//    for (let i = 0; i < global_register.length; i++){
+//        const element = global_register[i];
+//        var requestOptions = {
+//            method: 'POST',
 //             redirect: 'follow',
 //             // mode: 'no-cors',
 //           };
@@ -135,5 +409,6 @@ function status_generator(button) {
 //  };
 // }
 
-// TODO: zrob panel ktory pozwoli uruchomić lub zmodyfikować generator 
-// TODO: podpiąć button status do backendu
+// TODO: zrob panel ktory pozwoli uruchomić po raz pierwszy generator
+// TODO: zrób tak aby usuwał duplikujące się panele 
+// TODO: przycisk wznów
