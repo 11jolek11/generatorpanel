3301c1a262c62337e5af3218d2b94d88b2d05ad5
11jolek11 <dabrowskiandrzej094@gmail.com>
2022-12-25 19:04:18 +0100

    Generator component - base

--- panel/src/components/Generator.vue
+++ panel/src/components/Generator.vue
@@ -1,42 +1,78 @@
-<script>
+<script setup>
+    import { useGeneratorStore } from "../store/generator"
+    import axios from 'axios'
 
-import { ref } from 'vue'
-export default {
-    setup() {
-        return {}
+    const props = defineProps({
+        id: Number
+    })
+
+    const generators = useGeneratorStore()
+    const data = generators.register[props.id]
+
+
+    var new_data = {
+        data: {
+            source: '',
+            channel: '',
+            frequency: 0
+        },
+        MQTT: {
+            broker: '',
+            port: 0,
+            topic: ''
+        },
+        HTTP: {
+            host: '', 
+            port: 0
+        }
     }
-}
+
+    function start_generator(params) {
+        
+    }
+
+    function stop_generator(params){
+
+    }
+
+    function status_generator(params) {
+        
+    }
+
+
+
 </script>
 
 <template>
     <div class="item">
-            <input type="button" />
-            <p>Name: </p><p>Interface IP: </p>
-            <p>Port: </p>
-            <p>Interface id: </p>
-            <p>Config vector: </p>
             <button>TEXT</button>
-                <p ></p>
-            <input type="button" />
-            <input type="button" />
+            <p>Name: {{ data.config.name }}</p>
+            <p>Interface IP: {{ data.config.ip }}</p>
+            <p>Port: {{ data.config.port }}</p>
+            <p>Interface id: {{ data.config.uuid }}</p>
+            <p>Config vector: {{ data.config.config }}</p>
+            <button>Delete</button>
+            <p></p>
+            <button>Status</button>
+            <button>Stop</button>
             <div id="" class="common-edit">
                 <label for="channel">Channel: </label>
-                <input type="text" id="channel" class="_input"><br><br>
+                <input type="text" id="channel" class="_input" v-model="new_data.data.channel"><br><br>
                 <label for="frequency">Frequency: </label>
-                <input type="number" id="frequency" class="_input"><br><br>
+                <input type="number" id="frequency" class="_input" v-model="new_data.data.frequency"><br><br>
                 <p>MQTT</p>
                 <label for="mqttbroker">Broker adress: </label>
-                <input type="text" id="mqttbroker" class="_input"><br><br>
+                <input type="text" id="mqttbroker" class="_input" v-model="new_data.MQTT.broker"><br><br>
                 <label for="mqttport">Broker port</label>
-                <input type="number" id="mqttport" class="_input"><br><br>
+                <input type="number" id="mqttport" class="_input" v-model="new_data.MQTT.port"><br><br>
                 <label for="mqtttopic">Topic to publish on: </label>
-                <input type="text" id="mqtttopic" class="_input"><br><br>
+                <input type="text" id="mqtttopic" class="_input" v-model="new_data.MQTT.port"><br><br>
                 <p>HTTP</p>
                 <label for="httphost">Recipient IP adress: </label>
-                <input type="text" id="httphost" class="_input"><br><br>
+                <input type="text" id="httphost" class="_input" v-model="new_data.HTTP.host"><br><br>
                 <label for="httpport">Port: </label>
-                <input type="number" id="httpport" class="_input"><br><br>
-                <button type="submit">Send</button>
+                <input type="number" id="httpport" class="_input" v-model="new_data.HTTP.port"><br><br>
+                <button  type="submit">Send</button>
             </div>
         </div>
 </template>
