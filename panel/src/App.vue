783adea42a4977747e6ad806e1962630c248566b
11jolek11 <dabrowskiandrzej094@gmail.com>
2022-12-26 14:26:52 +0100

    App adeed

--- panel/src/App.vue
+++ panel/src/App.vue
@@ -1,26 +1,21 @@
 <script setup>
-import HelloWorld from "./components/HelloWorld.vue";
-import TheWelcome from "./components/TheWelcome.vue";
+import { onMounted } from "vue";
+// import HelloWorld from "./components/HelloWorld.vue";
+// import TheWelcome from "./components/TheWelcome.vue";
+import Generator from "./components/Generator.vue";
+import mqttRegistrator from "./registers/mqtt";
+import useAgregatorStore from "./store/agregator"
+import useGeneratorStore from "./store/generator"
+
+
+onMounted(function () {
+  var reg = mqttRegistrator()
+})
+
 </script>
 
 <template>
-  <header>
-    <img
-      alt="Vue logo"
-      class="logo"
-      src="./assets/logo.svg"
-      width="125"
-      height="125"
-    />
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
 </template>
 
 
